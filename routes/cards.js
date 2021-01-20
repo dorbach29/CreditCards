89e9890b3e03a6800822b28180f61b857856d8b1
DanielOrbach <dorbach29@gmail.com>
2021-01-19 22:16:14 -0500

    Basic changes to html structure

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const chk = require('chalk')
-
+const path = require("path");
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log(`cards.js : ${chk.gray("GET /cards")} for ${req.query.CardName}`);
-    res.send("Recived your request!")
+    try  { 
+        console.log(`cards.js : ${chk.gray("GET /cards")} for ${req.query.CardName}`);
+        res.sendFile(path.join(__dirname, '/../views/cards.html'));
+    } catch (err) {
+        console.log(chk.red(`cards.js: `) + err);
+        throw err;
+    }
 })
 
 module.exports = router; 
